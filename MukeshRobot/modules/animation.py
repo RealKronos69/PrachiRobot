c40f0321d4f5b4e8db05a2b0dfd0b5f27f6ceacc
Divyanshu Rana <141987294+Ranavanshi@users.noreply.github.com>
2023-09-22 03:36:02 +0530

    Update animation.py

--- MukeshRobot/modules/animation.py
+++ MukeshRobot/modules/animation.py
@@ -317,7 +317,7 @@ dispatcher.add_handler(BRAINANIMATION_HANDLER)
 
 __help__ = """
 
- ©️ [ᴍᴜᴋᴇsʜ] (f"tg://user?id={OWNER_ID}"))
+ ©️ [𝕯𝖎𝖛𝖞𝖆𝖓𝖘𝖍𝖚] (f"tg://user?id={OWNER_ID}"))
 
 *ғᴀᴋᴇ ᴀɴɪᴍᴀᴛɪᴏɴ ᴄᴏᴍᴍᴀɴᴅ*
  ❍ /love - ᴜsᴇ ɪᴛ ɪғ ᴜ ʜᴀᴠᴇ ɢɪʀʟғʀɪᴇɴᴅ
@@ -326,6 +326,6 @@ __help__ = """
  ❍ /kill  -  ᴛᴏ ᴋɪʟʟ  ᴜʀ ɢғ ᴅᴀᴅ
  ❍ /bombs -  ᴛᴏ sᴜᴄɪᴅᴇ ᴜʀsᴇʟғ
  ❍ /police - ᴛᴏ ᴄᴀʟʟ ᴍᴀsᴛᴇʀᴍɪɴᴅ ᴘᴏʟɪᴄᴇ
- ❍ ʙʏ ᴍᴜᴋᴇsʜ @Mukeshbotzone
+ ❍ ʙʏ ᴍᴜᴋᴇsʜ @webnet_bot_support
 """
 __mod_name__ = "ɢᴀᴍᴇ"
